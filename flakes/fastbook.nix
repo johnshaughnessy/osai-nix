898d279105691f6f8bade121dc52dac37e0aada2
John Shaughnessy <john@johnshaughnessy.com>
2023-10-31 13:09:17 -0400

    Add fastbook.nix flake

---
+++ flakes/fastbook.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/nixos-unstable"; # Default nixpkgs, change as needed
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [ ps.pip ]);
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [ pythonEnv ];
+          shellHook = ''
+            strippedPS1=$(echo -n "$PS1" | sed 's/^\\n//')
+
+            export PS1="(jupyter) $strippedPS1"
+
+            # Create a Python virtual environment
+            python -m venv .venv
+
+            # Activate the virtual environment
+            source .venv/bin/activate
+
+            # Install fastbook
+            pip install fastbook
+          '';
+        };
+      });
+
+}
