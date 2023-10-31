1343db41752906e3f746c19ba2c62ac019ca5576
John Shaughnessy <john@johnshaughnessy.com>
2023-10-31 11:17:31 -0400

    Add flake-utils

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -4,18 +4,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
-    nixosConfigurations.osai = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hardware-configuration.nix
-        ./configuration.nix
-        ./john-home-manager.nix
-        { imports = [ home-manager.nixosModules.home-manager ]; }
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, home-manager, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      nixosConfigurations = {
+        osai = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hardware-configuration.nix
+            ./configuration.nix
+            ./john-home-manager.nix
+            { imports = [ home-manager.nixosModules.home-manager ]; }
+          ];
+        };
+      };
+    });
 }
-
